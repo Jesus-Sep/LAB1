dc11b07526e21e4509d3149e748d22d3584c5bd3
jesus <jesus@correo.com>
2026-02-28 13:14:25 -0800

    comentar el codigo

--- test.py
+++ test.py
@@ -2,7 +2,10 @@ import qrcode
 from faker import Faker
 
 fake =Faker()
-#variables y constantes#
+#variables y constantes
+# Hice mas modificaciones para youtube, pero deje el codgio que uso par ael ciclo final
+#solo seria cambiar qr.add_data('Some data') por un link  o otra cosa
+#usamos csus propiedads para cmabiar color img = qr.make_image(fill_color="black", back_color="white")
 for _ in range(10):
     name = fake.name();
     img =qrcode.make(name)
